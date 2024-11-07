07af59b1502aa496ada56b9ba0860db1ed73667d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-11-06 23:57:42 -0800

    video.Video

--- video.go
+++ video.go
@@ -2,12 +2,14 @@ package video
 
 import (
 	"image"
+	"image/draw"
 	"time"
 )
 
 // Video is a series of image.Image.
 type Video interface {
 	Delay(int) time.Duration
+	DrawOperation(int) draw.Op
 	Image(int) image.Image
 	Len() int
 	LoopCount() int
